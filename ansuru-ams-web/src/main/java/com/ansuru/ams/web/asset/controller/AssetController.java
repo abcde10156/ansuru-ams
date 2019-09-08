86371929dacc3acd5da1d2baf70d495185d9f314
luotao <abcde10156@126.com>
2019-09-08 20:32:43 +0800

    321312

---
+++ ansuru-ams-web/src/main/java/com/ansuru/ams/web/asset/controller/AssetController.java
@@ -0,0 +1,18 @@
+package com.ansuru.ams.web.asset.controller;
+
+import com.ansuru.ams.svr.user.dto.request.RequestSvrUserFind;
+import com.ansuru.ams.svr.user.dto.response.ResponseSvrUserFind;
+import com.ansuru.ams.svr.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("asset")
+public class AssetController {
+
+    
+}
