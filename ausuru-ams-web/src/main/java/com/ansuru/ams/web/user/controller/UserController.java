86f96636109903536aeac33e7aee2e79cb8ad68a
luotao <abcde10156@126.com>
2019-09-08 16:44:10 +0800

    321321

--- ausuru-ams-web/src/main/java/com/ansuru/ams/web/user/controller/UserController.java
+++ ausuru-ams-web/src/main/java/com/ansuru/ams/web/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.ansuru.ams.web.user.controller;
 
+import com.ansuru.ams.common.utils.JsonUtils;
 import com.ansuru.ams.svr.user.dto.request.RequestSvrUserFind;
 import com.ansuru.ams.web.user.dto.RequestWebUserFind;
 import com.ansuru.ams.svr.user.dto.response.ResponseSvrUserFind;
 import com.ansuru.ams.web.user.dto.ResponseWebUserFind;
 import com.ansuru.ams.svr.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserService userService;
@@ -26,6 +31,7 @@ public class UserController {
         ResponseSvrUserFind entityUserPageInfo = userService.find(requestUserFind);
         ResponseWebUserFind responseWebUserFind = new ResponseWebUserFind();
         responseWebUserFind.setPageResult(entityUserPageInfo.getPageResult());
+        logger.info(JsonUtils.toJson(responseWebUserFind));
         return responseWebUserFind;
     }
 }
