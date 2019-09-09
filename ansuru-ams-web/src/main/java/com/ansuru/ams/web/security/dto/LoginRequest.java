61efe6f987bbf2a98f57d472005a99cd3da4e15c
luotao <abcde10156@126.com>
2019-09-09 18:25:34 +0800

    321312

--- ansuru-ams-web/src/main/java/com/ansuru/ams/web/security/dto/LoginRequest.java
+++ ansuru-ams-web/src/main/java/com/ansuru/ams/web/security/dto/LoginRequest.java
@@ -2,10 +2,14 @@ package com.ansuru.ams.web.security.dto;
 
 import com.ansuru.ams.common.dto.Request;
 
+import java.util.Date;
+
 public class LoginRequest extends Request {
     public String username;
 
     public String password;
+
+    private Date loginTime;
 
     public String getUsername() {
         return username;
@@ -21,5 +25,13 @@ public class LoginRequest extends Request {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Date getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Date loginTime) {
+        this.loginTime = loginTime;
     }
 }
