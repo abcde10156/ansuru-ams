5c98e6214d88a50a9ab780c1a4596e86c40d93e3
luotao <abcde10156@126.com>
2019-09-09 22:44:47 +0800

    321312

--- ansuru-ams-svr/src/main/java/com/ansuru/ams/MainApp.java
+++ ansuru-ams-svr/src/main/java/com/ansuru/ams/MainApp.java
@@ -3,15 +3,13 @@ package com.ansuru.ams;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.List;
-
 public class MainApp {
 
     public static void main(String[] args)throws Exception{
         ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        AssetMapper userService = (AssetMapper) app.getBean("assetMapper");
-        System.out.println("userService = " + userService);
+//        AssetMapper userService = (AssetMapper) app.getBean("assetMapper");
+//        System.out.println("userService = " + userService);
 
 //        Asset asset = new Asset();
 //        asset.setAssetName("test123");
@@ -19,13 +17,13 @@ public class MainApp {
 //        userService.insert(asset);
 //        System.out.println("asset1 = " + asset);
 
-        AssetExample assetExample = new AssetExample();
+//        AssetExample assetExample = new AssetExample();
 
-        assetExample.createCriteria() .andAssetNameEqualTo("adf12312");
-        assetExample.or().andAssetNameEqualTo("456");
+//        assetExample.createCriteria() .andAssetNameEqualTo("adf12312");
+//        assetExample.or().andAssetNameEqualTo("456");
 
-        List<Asset> assets = userService.selectByExample(assetExample);
-        System.out.println("assets = " + assets);
+//        List<Asset> assets = userService.selectByExample(assetExample);
+//        System.out.println("assets = " + assets);
 
     }
 }
