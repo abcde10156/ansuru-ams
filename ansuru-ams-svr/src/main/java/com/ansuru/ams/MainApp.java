157da6d84e77fba261e1a916083336f63762d04f
luotao <abcde10156@126.com>
2019-09-09 22:28:21 +0800

    321312

--- ansuru-ams-svr/src/main/java/com/ansuru/ams/MainApp.java
+++ ansuru-ams-svr/src/main/java/com/ansuru/ams/MainApp.java
@@ -1,15 +1,31 @@
 package com.ansuru.ams;
 
-import com.ansuru.ams.svr.user.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 public class MainApp {
 
     public static void main(String[] args)throws Exception{
         ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        UserService userService = (UserService) app.getBean("userService");
+        AssetMapper userService = (AssetMapper) app.getBean("assetMapper");
+        System.out.println("userService = " + userService);
+
+//        Asset asset = new Asset();
+//        asset.setAssetName("test123");
+//        asset.setAssetCode("321312");
+//        userService.insert(asset);
+//        System.out.println("asset1 = " + asset);
+
+        AssetExample assetExample = new AssetExample();
+
+        assetExample.createCriteria() .andAssetNameEqualTo("adf12312");
+        assetExample.or().andAssetNameEqualTo("456");
+
+        List<Asset> assets = userService.selectByExample(assetExample);
+        System.out.println("assets = " + assets);
 
     }
 }
